2ead759577e427007a581c896a536c87f0bd6a7e
Daniel <danielfdoficial@gmail.com>
2023-11-16 14:39:29 -0300

    refac: add app router to next-js

--- components/HeaderCartButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import { HeaderCartIcon } from "@/assets/Vector";
-
-export default function HeaderCartButton() {
-  return (
-    <button>
-      <HeaderCartIcon/> <span>0</span>
-    </button>
-  )
-}
